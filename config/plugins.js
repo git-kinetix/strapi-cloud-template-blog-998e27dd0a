4d9eaa5edf249df494e89dc4832d82f028c937aa
Ulysse Mizrahi <ulysse@mindmesh.com>
2025-10-08 08:58:12 -0700

    cache

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,13 @@
-module.exports = () => ({});
+module.exports = () => ({
+  upload: {
+    enabled: true,
+    config: {
+      providerOptions: {
+        localServer: {
+          // 30 days
+          maxage: 30 * 24 * 60 * 60 * 1000,
+        },
+      },
+    },
+  },
+});
